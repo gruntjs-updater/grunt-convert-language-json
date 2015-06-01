d6dc4c748eeed9a69fecdf455911ff1ae33028db
Sebastian Schrepfer <sebschrepfer@hotmail.com>
2015-06-01 11:19:59 +0200

    grunt init

---
+++ Gruntfile.js
@@ -0,0 +1,69 @@
+/*
+ * grunt-convert-language-json
+ * https://github.com/ssc-hrep3/grunt-convert-language-json
+ *
+ * Copyright (c) 2015 Sebastian Schrepfer
+ * Licensed under the Apache, 2.0 licenses.
+ */
+
+'use strict';
+
+module.exports = function(grunt) {
+
+	// Project configuration.
+	grunt.initConfig({
+		jshint: {
+			all: ['Gruntfile.js', 'tasks/*.js', '<%= nodeunit.tests %>'],
+			options: {
+				jshintrc: '.jshintrc'
+			}
+		},
+
+		// Before generating any new files, remove any previously-created files.
+		clean: {
+			tests: ['tmp']
+		},
+
+		// Configuration to be run (and then tested).
+		convert_language_json: {
+			default_options: {
+				options: {
+				},
+				files: {
+					'tmp/default_options': ['test/fixtures/testing', 'test/fixtures/123']
+				}
+			},
+			custom_options: {
+				options: {
+					separator: ': ',
+					punctuation: ' !!!'
+				},
+				files: {
+					'tmp/custom_options': ['test/fixtures/testing', 'test/fixtures/123']
+				}
+			}
+		},
+
+		// Unit tests.
+		nodeunit: {
+			tests: ['test/*_test.js']
+		}
+
+	});
+
+	// Actually load this plugin's task(s).
+	grunt.loadTasks('tasks');
+
+	// These plugins provide necessary tasks.
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-contrib-nodeunit');
+
+	// Whenever the "test" task is run, first clean the "tmp" dir, then run this
+	// plugin's task(s), then test the result.
+	grunt.registerTask('test', ['clean', 'convert_language_json', 'nodeunit']);
+
+	// By default, lint and run all tests.
+	grunt.registerTask('default', ['jshint', 'test']);
+
+};
